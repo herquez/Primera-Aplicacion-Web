37c821989214ab7049f48dea668362d26f15c7d6
herquez <miguelherquez@gmail.com>
2022-08-25 01:45:08 -0500

    Busqueda sensitiva en cliente

---
+++ Primera Aplicacion Web/Scripts/Cliente.js
@@ -0,0 +1,12 @@
+﻿
+function itemToDelete(idCliente) {
+    document.getElementById("idCliente").value = idCliente;
+}
+
+function borrar() {
+    document.getElementById("deleteForm").submit();
+}
+
+document.getElementById("searchName").onkeyup = function () {
+    document.getElementById("searchForm").submit();
+}
