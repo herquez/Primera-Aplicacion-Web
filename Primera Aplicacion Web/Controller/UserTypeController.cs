ed076b8aa44b07f98799ad6f125bf9465861b2c5
herquez <miguelherquez@gmail.com>
2022-09-03 13:17:03 -0500

    Se crearon el modelo, controlador e indice de los tipos de usuario.

---
+++ Primera Aplicacion Web/Controller/UserTypeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Primera_Aplicacion_Web.Models;
+
+namespace Primera_Aplicacion_Web.Controllers
+{
+    public class UserTypeController : Controller
+    {
+        // GET: UserType
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create() {
+            return View();
+        }
+    }
+}
