46705a2b3fe51bbc75ff2b8e3b17b37aeeb85d5a
herquez <miguelherquez@gmail.com>
2022-09-05 19:48:31 -0500

    Corrección de variable focus que se intentó usar para estableces el puntero en la busqueda sensitiva.

--- Primera Aplicacion Web/Controller/MarcaController.cs
+++ Primera Aplicacion Web/Controller/MarcaController.cs
@@ -24,7 +24,6 @@ namespace Primera_Aplicacion_Web.Controllers
                                   nombre = marca.NOMBRE,
                                   descripcion = marca.DESCRIPCION,
                               }).ToList();
-                    ViewBag.focus = false;
                 } else {
                     listaMarca = (from marca in bd.Marca
                                   where marca.BHABILITADO == 1 &&
@@ -34,9 +33,7 @@ namespace Primera_Aplicacion_Web.Controllers
                                       nombre = marca.NOMBRE,
                                       descripcion = marca.DESCRIPCION,
                                   }).ToList();
-                    ViewBag.focus = true;
                 }
-                
             }
             return View(listaMarca);
         }
