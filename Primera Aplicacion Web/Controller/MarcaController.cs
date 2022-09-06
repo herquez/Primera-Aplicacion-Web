61504da3bf466e0448a061acc26eaba320a24c3c
herquez <miguelherquez@gmail.com>
2022-09-05 19:44:21 -0500

    Revert "Corrección de variable focus que se intentó usar para estableces el puntero en la busqueda sensitiva."
    This reverts commit e5409398990092da7dbcce5698b058da91d4abcf.

--- Primera Aplicacion Web/Controller/MarcaController.cs
+++ Primera Aplicacion Web/Controller/MarcaController.cs
@@ -24,6 +24,7 @@ namespace Primera_Aplicacion_Web.Controllers
                                   nombre = marca.NOMBRE,
                                   descripcion = marca.DESCRIPCION,
                               }).ToList();
+                    ViewBag.focus = false;
                 } else {
                     listaMarca = (from marca in bd.Marca
                                   where marca.BHABILITADO == 1 &&
@@ -33,7 +34,9 @@ namespace Primera_Aplicacion_Web.Controllers
                                       nombre = marca.NOMBRE,
                                       descripcion = marca.DESCRIPCION,
                                   }).ToList();
+                    ViewBag.focus = true;
                 }
+                
             }
             return View(listaMarca);
         }
