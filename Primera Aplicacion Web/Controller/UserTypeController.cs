ffc416f9e84ad1e3e2865f3e69f5ea418b71a608
herquez <miguelherquez@gmail.com>
2023-06-27 20:00:05 -0600

    Add multi column searching. create global varibale and filter.

--- Primera Aplicacion Web/Controller/UserTypeController.cs
+++ Primera Aplicacion Web/Controller/UserTypeController.cs
@@ -9,31 +9,56 @@ namespace Primera_Aplicacion_Web.Controllers
 {
     public class UserTypeController : Controller
     {
+        private UserTypeCLS globalUserType;
+
         // GET: UserType
         public ActionResult Index(UserTypeCLS userTypeCLS)
         {
+            globalUserType = userTypeCLS;
             List<UserTypeCLS> userTypeList = new List<UserTypeCLS>();
-            using(var bd = new BDPasajeEntities()) {
-                if(userTypeCLS.nombre == null) {
-                    userTypeList = (from userType in bd.TipoUsuario
-                                  where userType.BHABILITADO == 1
-                                  select new UserTypeCLS {
-                                      iidtipousuario = userType.IIDTIPOUSUARIO,
-                                      nombre = userType.NOMBRE,
-                                      descripcion = userType.DESCRIPCION,
-                                  }).ToList();
+            List<UserTypeCLS> userTypeFiltered = new List<UserTypeCLS>();
+            using (var bd = new BDPasajeEntities()) {
+                userTypeList = (from userType in bd.TipoUsuario
+                                where userType.BHABILITADO == 1
+                                select new UserTypeCLS
+                                {
+                                    iidtipousuario = userType.IIDTIPOUSUARIO,
+                                    nombre = userType.NOMBRE,
+                                    descripcion = userType.DESCRIPCION,
+                                }).ToList();
+
+                if (userTypeCLS.iidtipousuario == 0 && 
+                    userTypeCLS.nombre == null && 
+                    userTypeCLS.descripcion == null) {
+                    userTypeFiltered = userTypeList;
                 } else {
-                    userTypeList = (from userType in bd.TipoUsuario
-                                  where userType.BHABILITADO == 1 &&
-                                  userType.NOMBRE.Contains(userTypeCLS.nombre)
-                                  select new UserTypeCLS {
-                                      iidtipousuario = userType.IIDTIPOUSUARIO,
-                                      nombre = userType.NOMBRE,
-                                      descripcion = userType.DESCRIPCION,
-                                  }).ToList();
+                    Predicate<UserTypeCLS> pred = new Predicate<UserTypeCLS>(searchUserType);
+                    userTypeFiltered = userTypeList.FindAll(pred);
                 }
             }
-            return View(userTypeList);
+            return View(userTypeFiltered);
+        }
+
+        private bool searchUserType(UserTypeCLS userTypeCLS)
+        {
+            bool searchId = true;
+            bool searchName = true;
+            bool searchDescription = true;
+
+            if(globalUserType.iidtipousuario > 0)
+            {
+                searchId = userTypeCLS.iidtipousuario.ToString().Contains(globalUserType.iidtipousuario.ToString());
+            }
+            if(globalUserType.nombre != null)
+            {
+                searchName = userTypeCLS.nombre.ToString().Contains(globalUserType.nombre);
+            }
+            if(globalUserType.descripcion != null)
+            {
+                searchDescription = userTypeCLS.descripcion.ToString().Contains(globalUserType.descripcion);
+            }
+
+            return (searchId && searchName && searchDescription);
         }
 
         public ActionResult Create() {
