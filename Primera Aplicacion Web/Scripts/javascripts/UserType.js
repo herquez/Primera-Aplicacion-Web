fd91ba2fa9c2296c98f836046ac6c53cfcfa7631
herquez <miguelherquez@gmail.com>
2023-06-27 19:59:26 -0600

    Add javascript for the user type section. Submit forms for multi column searching.

---
+++ Primera Aplicacion Web/Scripts/javascripts/UserType.js
@@ -0,0 +1,17 @@
+﻿var iidtipousuario = document.getElementById("iidtipousuario");
+var nombre = document.getElementById("nombre");
+var descripcion = document.getElementById("descripcion");
+
+var search_form = document.getElementById("search_form");
+
+iidtipousuario.onkeyup = function () {
+    search_form.submit();
+}
+
+nombre.onkeyup = function () {
+    search_form.submit();
+}
+
+descripcion.onkeyup = function () {
+    search_form.submit();
+}
